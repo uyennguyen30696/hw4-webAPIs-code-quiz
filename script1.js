32e8cdca8f2ee2626781a2074d6df9dfeee55b54
Uyen Nguyen <udnguyen@ucsd.edu>
2020-10-26 02:30:58 -0700

    updatequestion logic

--- script1.js
+++ script1.js
@@ -61,6 +61,10 @@ function setTime() {
     },1000); 
 }
 
+// countdownButton.addEventListener("click", function() {
+//     setTime();
+// });
+
 // Questions
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 var questions = [
@@ -90,6 +94,99 @@ var questions = [
         answer: "B. Bacterial"
     },
 ]
+// From questions.html
+var questionWrapperEl = document.querySelector(".question-wrapper");
+var h1QuestionEl = document.querySelector("#question");
+
+// Store body as a variable
+var body = document.body;
+
+// Create buttons for answers in each question
+var choiceA = document.createElement("button");
+// body.questionWrapperEl.children[1].children[0].createElement("button") = choiceA;
+choiceA.setAttribute("class", "btn");
+choiceA.setAttribute("id", "choiceA");
+
+var choiceB = document.createElement("button");
+choiceB.setAttribute("class", "btn");
+choiceB.setAttribute("id", "choiceB");
+
+var choiceC = document.createElement("button");
+choiceC.setAttribute("class", "btn");
+choiceC.setAttribute("id", "choiceC");
+
+// When click start button, setTime() runs to countdown time and questions are presented
+countdownButton.addEventListener("click", function() {
+    setTime();
+
+    // First question presented
+    for (i= 0; i < questions.length; i++) {
+        body.appendChild(choiceA);
+        body.appendChild(choiceB);
+        body.appendChild(choiceC);
+        // Present question
+        h1QuestionEl.textContent = questions[i].question;
+        // Present choices
+        choiceA.textContent = questions[i].choices[0];
+        choiceB.textContent = questions[i].choices[1];
+        choiceC.textContent = questions[i].choices[2];
+
+        // When user clicked to make choices
+        $("#choiceA").on("click", function() {
+            loadNextQuestion();
+        });
+        $("#choiceB").on("click", function() {
+            loadNextQuestion();
+        });
+        $("#choiceC").on("click", function() {
+            loadNextQuestion();
+        });
+        
+        // Load next question function
+        function loadNextQuestion() {
+            for (i= 0; i < questions.length; i++) {
+                // Get user choice
+                var userChoice = this.innerHTML;
+
+                // If correct answer is selected
+                // Plus one score for one correct answer
+                if (userChoice === questions[i].answer) {
+                    score++;
+                    i++;
+
+                    // Present question
+                    h1QuestionEl.textContent = questions[i].question;
+                    // Present choices
+                    choiceA.textContent = questions[i].choices[0];
+                    choiceB.textContent = questions[i].choices[1];
+                    choiceC.textContent = questions[i].choices[2];
+                }
+                // If incorrect answer is selected (else)
+                // Subtract 5 sec in the countdown timer for each incorrect answer
+                else if (userChoice !== questions[i].answer) {
+                    secondsElapse -= 5;
+                    i++;
+
+                    // Present question
+                    h1QuestionEl.textContent = questions[i].question;
+                    // Present choices
+                    choiceA.textContent = questions[i].choices[0];
+                    choiceB.textContent = questions[i].choices[1];
+                    choiceC.textContent = questions[i].choices[2];
+                }
+            }
+        }
+    }
+     
+});
+
+// When game ended
+// If timer over
+// If reach the last question before time is over
+
+// Enter name and save score
+
+// Reset button at the end of the game
 
 
 
