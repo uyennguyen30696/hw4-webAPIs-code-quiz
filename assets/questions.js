cb8301d441e001841c12d0fe4496fb1805067fa8
Uyen Nguyen <udnguyen@ucsd.edu>
2020-12-23 18:26:41 -0800

    add question number

--- assets/questions.js
+++ assets/questions.js
@@ -1,26 +1,26 @@
 var questions = [
     {
-        question: "What is the first element on the periodic table?",
+        question: "1. What is the first element on the periodic table?",
         choices: ["A. Hydrogen", "B. Helium", "C. Carbon"],
         answer: "A. Hydrogen"
     },
     {
-        question: "Acid has pH above 7",
+        question: "2. Acid has pH above 7",
         choices: ["A. True", "B. False", "C. pH = 7"],
         answer: "B. False"
     },
     {
-        question: "Chemical symbol for gold is:",
+        question: "3. Chemical symbol for gold is:",
         choices: ["A. Au", "B. Cu", "C. Gu"],
         answer: "A. Au"
     },
     {
-        question: "What is the charge of an electron?",
+        question: "4. What is the charge of an electron?",
         choices: ["A. Neutral", "B. Positive", "C. Negative"],
         answer: "C. Negative"
     },
     {
-        question: "In human, antibiotic is used to treat what kind of infection?",
+        question: "5. In human, antibiotic is used to treat what kind of infection?",
         choices: ["A. Viral", "B. Bacterial", "C. Both viral and bacterial"],
         answer: "B. Bacterial"
     },
